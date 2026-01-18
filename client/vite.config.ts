384a175775dc9be13da0e111b71ac9fe7c7144c7
Corelink Data <corelinkdata@gmail.com>
2026-01-18 23:34:28 +0700

    Implement manual PWA support (Add to Home Screen)

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // VitePWA plugin removed due to build issues. Manual manifest used.
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
